bc3a3f95fb614403c0ef8a1c130bd34a923e750d
ArvinAjifTechology <arvinajif3@gmail.com>
2023-07-16 19:44:04 +0700

    Menambahkan Item Model

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -21,63 +21,13 @@ class Item extends Model
         'created' => ItemCreated::class,
     ];
 
-    public static function getAll()
-    {
-        return DB::table('items')
-            ->join('rooms', 'items.room_id', '=', 'rooms.id')
-            ->select('items.*', "rooms.room_name AS room_name")
-            ->get();
-    }
-
     public static function getAllForOperator()
     {
-        // return DB::table('items')
-        //     ->join('rooms', 'items.room_id', '=', 'rooms.id')
-        //     ->where('rooms.id', Auth::user()->rooms->id)
-        //     ->select('items.*', "rooms.room_name AS room_name")
-        //     ->get();
         return Item::whereHas('room', function ($query) {
             $query->where('user_id', Auth::id());
         })->get();
     }
 
-    public static function findId($id)
-    {
-        return DB::select('select * from items where id = ?', [$id]);
-    }
-
-    public static function insert($request)
-    {
-        DB::insert('INSERT INTO items (item_name,room_id,description, `condition`, rental_price, late_fee_per_day, quantity) VALUES (?, ?, ?, ?, ?, ?, ?)', [
-            $request->input('item_name'),
-            $request->input('room_id'),
-            $request->input('description'),
-            $request->input('condition'),
-            $request->input('rental_price'),
-            $request->input('late_fee_per_day'),
-            $request->input('quantity'),
-        ]);
-    }
-
-    public static function edit($request)
-    {
-        DB::insert('UPDATE items SET item_code= ? ,item_name= ? ,room_id= ? ,description= ? , `condition`= ? , rental_price= ?, late_fee_per_day= ?, quantity= ? WHERE id = ?', [
-            $request->input('item_code'),
-            $request->input('item_name'),
-            $request->input('room_id'),
-            $request->input('description'),
-            $request->input('condition'),
-            $request->input('rental_price'),
-            $request->input('late_fee_per_day'),
-            $request->input('quantity'),
-            $request->input('id')
-        ]);
-    }
-
-    public static function destroy($id)
-    {
-        DB::delete('DELETE FROM items WHERE id = ?', [$id]);
-    }
 
     public function room()
     {
