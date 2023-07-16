cf95e39a7855d9592ad7e0c10b4f6d94294f0b2a
ArvinAjifTechology <arvinajif3@gmail.com>
2023-07-16 19:43:01 +0700

    Menambahkan Room

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -5,10 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Room;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
-
 use Illuminate\Support\Facades\Validator;
 
 class RoomController extends Controller
@@ -18,17 +15,14 @@ class RoomController extends Controller
      */
     public function index()
     {
-        // $rooms= DB::select('select * from rooms');
         if (Gate::allows('admin')) {
-            // akses yang diizinkan untuk admin
-            $rooms = Room::getAll(); //model
-            $rooms = collect($rooms);
+            $rooms = Room::getAll();
         } elseif (Gate::allows('operator')) {
             $rooms = Room::getAllForOperator();
-            $rooms = collect($rooms);
         } else {
             abort(403, 'Unauthorized');
         }
+
         return view('rooms.index', compact('rooms'));
     }
 
@@ -38,12 +32,11 @@ class RoomController extends Controller
     public function create()
     {
         if (Gate::allows('admin')) {
-            // akses yang diizinkan untuk admin
-            $users = User::latest()->where('role', '=', 'operator')->get();
+            $users = User::where('role', 'operator')->get();
         } else {
             abort(403, 'Unauthorized');
         }
-        // $user = Collection::make($user);
+
         return view('rooms.create', compact('users'));
     }
 
@@ -53,8 +46,7 @@ class RoomController extends Controller
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'room_name' => ['required', 'string:50', Rule::unique('rooms')],
-            // 'room_code' => ['required', 'string', Rule::unique('rooms')],
+            'room_name' => ['required', 'string', 'max:50', 'unique:rooms'],
             'user_id' => ['required', 'integer'],
             'description' => ['required', 'string'],
         ]);
@@ -65,9 +57,9 @@ class RoomController extends Controller
                 ->withInput();
         }
 
-        Room::insert($request);
+        Room::create($request->all());
 
-        return redirect('/admin/rooms')->withErrors($validator)->withInput()->with('status', 'Selamat Data Berhasil Di Tambahkan');
+        return redirect('/admin/rooms')->with('status', 'Data berhasil ditambahkan');
     }
 
     /**
@@ -76,8 +68,7 @@ class RoomController extends Controller
     public function show(string $id)
     {
         if (Gate::allows('admin')) {
-            // akses yang diizinkan untuk admin
-            $room = Room::find($id); //model
+            $room = Room::find($id);
         } elseif (Gate::allows('operator')) {
             $room = Room::find($id);
         } else {
@@ -92,8 +83,9 @@ class RoomController extends Controller
      */
     public function edit(string $id)
     {
-        $room = Room::findId($id);
-        $users = User::latest()->where('role', 'operator')->get();
+        $room = Room::find($id);
+        $users = User::where('role', 'operator')->get();
+
         return view('rooms.edit', compact('room', 'id', 'users'));
     }
 
@@ -102,22 +94,23 @@ class RoomController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        $id = $request->input('id');
-        $room = DB::select('select * from rooms where id = ?', [$id]);
+        $room = Room::find($id);
+
         $validator = Validator::make($request->all(), [
-            'room_name' => ['required', 'string', Rule::unique('rooms')->ignore($id)],
+            'room_name' => ['required', 'string', 'max:50', 'unique:rooms,room_name,' . $id],
             'user_id' => ['required', 'integer'],
             'description' => ['required', 'string'],
         ]);
+
         if ($validator->fails()) {
             return redirect('/admin/rooms/' . $id . '/edit')
                 ->withErrors($validator)
                 ->withInput();
         }
-        $fullName = $request->input('first_name') . $request->input('last_name');
-        // Mengupdate User
-        Room::edit($request);
-        return redirect('admin/rooms')->withErrors($validator)->with('status', 'Selamat Data Berhasil Di Update')->withInput();
+
+        $room->update($request->all());
+
+        return redirect('/admin/rooms')->with('status', 'Data berhasil diperbarui');
     }
 
     /**
@@ -125,20 +118,17 @@ class RoomController extends Controller
      */
     public function destroy(string $id)
     {
-        $room = DB::selectOne('SELECT * FROM rooms WHERE id = ?', [$id]);
+        $room = Room::find($id);
 
         if ($room) {
-            $roomItems = DB::delete('DELETE FROM items WHERE room_id = ?', [$room->id]);
-            $roomUser = DB::delete('DELETE FROM users WHERE id = ?', [$room->user_id]);
-
-            DB::delete('DELETE FROM rooms WHERE id = ?', [$id]);
+            $room->items()->delete();
+            $room->delete();
         }
 
-
         if (Gate::allows('admin')) {
-            return redirect('/admin/rooms')->with('status', 'Data berhasil Di Hapus');
+            return redirect('/admin/rooms')->with('status', 'Data berhasil dihapus');
         } elseif (Gate::allows('operator')) {
-            return redirect('/oprator/rooms')->with('status', 'Data berhasil Di Hapus');
+            return redirect('/oprator/rooms')->with('status', 'Data berhasil dihapus');
         } else {
             abort(403, 'Unauthorized');
         }
