b8a79487b432322c012a1391482d2ce68c954c1e
ArvinAjifTechology <arvinajif3@gmail.com>
2023-07-16 19:48:25 +0700

    Update API Routes

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\Api\BorrowController;
+use App\Http\Controllers\API\BorrowController;
 use App\Http\Controllers\API\ItemController;
 use App\Http\Controllers\API\RoomController;
 
@@ -26,14 +26,13 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('users', UserController::class)->middleware('admin');
     Route::apiResource('rooms', RoomController::class);
     Route::apiResource('items', ItemController::class);
-    Route::apiResource('dashboard-admin', AdminDashboardController::class)->middleware('admin');
-
     Route::apiResource('borrows', BorrowController::class);
+    Route::apiResource('dashboard-admin', AdminDashboardController::class)->middleware('admin');
     Route::post('borrows/{id}/return', [BorrowController::class, 'returnBorrow']);
     Route::get('borrows/send-reminder', [BorrowController::class, 'sendReturnReminder']);
     Route::get('borrows/submit-request/{borrow_code}', [BorrowController::class, 'submitBorrowRequest']);
-    Route::post('borrows/verify-request/{borrow_code}', [BorrowController::class, 'verifySubmitBorrowRequest']);
-    Route::post('borrows/{id}/reject-request', [BorrowController::class, 'rejectBorrowRequest']);
+    Route::put('borrows/verify-request/{borrow_code}', [BorrowController::class, 'verifySubmitBorrowRequest']);
+    Route::put('borrows/{id}/reject-request', [BorrowController::class, 'rejectBorrowRequest']);
 });
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
